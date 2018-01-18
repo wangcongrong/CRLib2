51430da604af7191571b298bd791f23cb1a8821c
congrong <1349375254@qq.com>
2018-01-18 11:46:00 +0800

    初始化库文件

---
+++ CRLib2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CRLib2
+//
+//  Created by 王从容 on 2018/1/18.
+//  Copyright © 2018年 王从容. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CRLib2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CRLib2
+//
+//  Created by 王从容 on 2018/1/18.
+//  Copyright © 2018年 王从容. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
